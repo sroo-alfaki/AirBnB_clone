f54d3c4ea594b443c4f331176ddddd054ade8af3
saria <srooalfakiahmed@gmail.com>
2024-03-24 18:59:28 +0300

    AirBnB_clone

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing on an empty line or ENTER shouldn’t execute anything"""
+    
+    def do_EOF(self, line):
+        """EOF to exit the program"""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """quit to exit the program"""
+        print()
+        return True
+
+    def help_quit(self):
+        """Help message for the quit command."""
+        print("Quit command to exit the program\n")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
+
+
