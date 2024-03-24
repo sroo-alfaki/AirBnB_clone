c1cd26a372497d78d4864d32e0484f19b037b9e5
saria <srooalfakiahmed@gmail.com>
2024-03-24 18:41:22 +0300

    AirBnB_clone

--- models/base_model.py
+++ models/base_model.py
@@ -17,3 +17,26 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+    def __str__(self) -> str:
+        """Return a string representation of the instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """Update the updated_at attribute and save the instance."""
+        self.updated_at = datetime.now()
+        models.storage.save()
+        models.storage.new(self)
+
+    def to_dict(self) -> dict:
+        """Return a dictionary of instance attributes."""
+        excluded = ['name', 'my_number']
+        result = {i: y for i, y in self.__dict__.items() if i not in excluded}
+        result['__class__'] = self.__class__.__name__
+
+        for i, y in result.items():
+            if isinstance(y, datetime):
+                result[i] = y.isoformat()
+
+        return result
+
