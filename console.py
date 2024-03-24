c255d61f2ceb48880f5226b602c2c1aab05c203b
saria <srooalfakiahmed@gmail.com>
2024-03-24 19:11:46 +0300

    AirBnB_clone

--- console.py
+++ console.py
@@ -2,6 +2,14 @@
 """The cmd Module
 """
 import cmd
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from datetime import datetime
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
